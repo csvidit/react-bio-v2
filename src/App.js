544e118baa37bef1dc284faf6323e1321cc49569
Vidit Khandelwal <viditk17@gmail.com>
2022-10-26 01:14:43 +0100

    testing

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import ProfileContainer from "./components/ProfileContainer";
 import './App.css';
 import React from "react";
 import CoverImage from "./components/CoverImage";
+import Spotlight from "./components/Spotlight";
+import Footer from "./components/Footer";
 
 // function App() {
 //   return (
@@ -17,13 +19,15 @@ import CoverImage from "./components/CoverImage";
 class App extends React.Component {
   render() {
     return (
-      <div className="App main-body bg-fixed">
+      <div className="App bg-fixed">
         <div className="flex flex-col items-center snap-y bg-fixed">
           <CoverImage></CoverImage>
           <ProfileContainer></ProfileContainer>
+          <Spotlight></Spotlight>
           <ListLinkItem linkType="ghR" link="https://github.com/csvidit" linkText="csvidit"></ListLinkItem>
           <ListLinkItem linkType="ghR" link="https://github.com/csvidit" linkText="csvidit"></ListLinkItem>
           <ListLinkItem linkType="litP" link="https://github.com/csvidit" linkText="MMXXII"></ListLinkItem>
+          <Footer></Footer>
         </div>
       </div>
     );
---
+++ src/components/Footer.js
@@ -0,0 +1,6 @@
+const Footer = () => {
+    return (<div><p className="mt-10 text-center text-white">© 2022 Vidit Khandelwal. All rights reserved.</p>
+        <a href="https://github.com/csvidit/bio-v2" rel="noreferrer" target="_blank"><p className="text-center underline decoration-sky-500 hover:text-slate-300 text-white">Go to this project's GitHub repository for more information.</p></a></div>);
+}
+
+export default Footer;
