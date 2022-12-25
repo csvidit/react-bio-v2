4c4675de2fd7c23f4b418be2563b490ca3765fb2
Vidit Khandelwal <viditk17@gmail.com>
2022-12-26 00:24:33 +0530

    added link

--- src/App.js
+++ src/App.js
@@ -25,6 +25,7 @@ class App extends React.Component {
           <ProfileContainer></ProfileContainer>
           <Spotlight></Spotlight>
           <ListLinkItem linkType="ghR" link="https://github.com/csvidit/react-bio-v2" linkText="react-bio-v2"></ListLinkItem>
+          <ListLinkItem linkType="ghR" link="https://github.com/csvidit/nextjs-main-landing" linkText="nextjs-main-landing"></ListLinkItem>
           <ListLinkItem linkType="ghR" link="https://github.com/csvidit/bio-v2" linkText="bio-v2"></ListLinkItem>
           <ListLinkItem linkType="litP" link="https://github.com/csvidit" linkText="MMXXII"></ListLinkItem>
           <Footer></Footer>
