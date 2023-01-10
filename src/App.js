ffbc2cb597266efaaad9b0363b64b767b47aceb5
Vidit Khandelwal <viditk17@gmail.com>
2023-01-10 14:26:31 +0530

    links now fetched from a database via graphql

--- src/App.js
+++ src/App.js
@@ -1,37 +1,59 @@
 import ListLinkItem from "./components/ListLinkItem";
+import ListLinkItems from "./components/ListLinkItems";
 import ProfileContainer from "./components/ProfileContainer";
-import './App.css';
+import "./App.css";
 import React from "react";
 import CoverImage from "./components/CoverImage";
 import Spotlight from "./components/Spotlight";
 import Footer from "./components/Footer";
-import { getDatabase } from "firebase/database";
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <ProfileContainer></ProfileContainer>
-//       <ListLinkItem linkType="ghR" link="https://github.com/csvidit" linkText="csvidit"></ListLinkItem>
-//       <ListLinkItem linkType="ghR" link="https://github.com/csvidit" linkText="csvidit"></ListLinkItem>
-//     </div>
-//   );
-// }
+import { getDatabase, ref, onValue, getData } from "firebase/database";
+import { Suspense, useEffect, useState } from "react";
+import { db } from "./firebase";
 
 class App extends React.Component {
   render() {
+    // const db = getDatabase();
+    // const data = ref(getDatabase, "vk-bio-default-rtdb");
+    // onValue(data, (snapshot) => {
+    //   console.log([snapshot.val()]);
+    // });
+    // useEffect(() => {
+    //   getData();
+    // });
     return (
-      <div className="App bg-fixed">
-        <div className="flex flex-col items-center snap-y bg-fixed">
-          <CoverImage></CoverImage>
-          <ProfileContainer></ProfileContainer>
-          <Spotlight></Spotlight>
-          <ListLinkItem linkType="ghR" link="https://github.com/csvidit/react-bio-v2" linkText="react-bio-v2"></ListLinkItem>
-          <ListLinkItem linkType="ghR" link="https://github.com/csvidit/nextjs-main-landing" linkText="nextjs-main-landing">l</ListLinkItem>
-          <ListLinkItem linkType="ghR" link="https://github.com/csvidit/bio-v2" linkText="bio-v2"></ListLinkItem>
-          <ListLinkItem linkType="litP" link="https://github.com/csvidit" linkText="MMXXII"></ListLinkItem>
-          <Footer></Footer>
+      <React.StrictMode>
+        <div className="App bg-fixed">
+          <div className="flex snap-y flex-col items-center bg-fixed">
+            <CoverImage></CoverImage>
+            <ProfileContainer></ProfileContainer>
+            <Spotlight></Spotlight>
+            {/* <ListLinkItem
+              linkType="ghR"
+              link="https://github.com/csvidit/react-bio-v2"
+              linkText="react-bio-v2"
+            ></ListLinkItem>
+            <ListLinkItem
+              linkType="ghR"
+              link="https://github.com/csvidit/nextjs-main-landing"
+              linkText="nextjs-main-landing"
+            >
+              l
+            </ListLinkItem>
+            <ListLinkItem
+              linkType="ghR"
+              link="https://github.com/csvidit/bio-v2"
+              linkText="bio-v2"
+            ></ListLinkItem>
+            <ListLinkItem
+              linkType="litP"
+              link="https://github.com/csvidit"
+              linkText="MMXXII"
+            ></ListLinkItem> */}
+            <ListLinkItems></ListLinkItems>
+            <Footer></Footer>
+          </div>
         </div>
-      </div>
+      </React.StrictMode>
     );
   }
 }
--- src/firebase.js
+++ src/firebase.js
@@ -1,65 +1,46 @@
-// Import the functions you need from the SDKs you need
+// // Import the functions you need from the SDKs you need
 
-import { initializeApp } from "firebase/app";
+// import { initializeApp } from "firebase/app";
 
-import { getAnalytics } from "firebase/analytics";
+// import { getAnalytics } from "firebase/analytics";
 
-import { getDatabase } from "firebase/database";
+// import { getDatabase } from "firebase/database";
 
-require('dotenv').config();
-
-
-// TODO: Add SDKs for Firebase products that you want to use
-
-// https://firebase.google.com/docs/web/setup#available-libraries
+// // require('dotenv').config();
 
 
-// Your web app's Firebase configuration
+// // TODO: Add SDKs for Firebase products that you want to use
 
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+
+// // Your web app's Firebase configuration
+
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
 // const firebaseConfig = {
 
-//   apiKey: "AIzaSyDbUQ67APOvHL5jD00YaKde3MGltLcPcBQ",
-
-//   authDomain: "vk-bio.firebaseapp.com",
-
-//   projectId: "vk-bio",
-
-//   storageBucket: "vk-bio.appspot.com",
-
-//   messagingSenderId: "1082610852982",
-
-//   appId: "1:1082610852982:web:60bb548d248be78ffbab57",
-
-//   measurementId: "G-9DECLNWHZT"
-
-// };
-
-const firebaseConfig = {
-
-    apiKey: process.env.REACT_APP_APIKEY,
+//     apiKey: process.env.REACT_APP_APIKEY,
   
-    authDomain: process.env.REACT_APP_DOMAIN,
+//     authDomain: process.env.REACT_APP_DOMAIN,
   
-    projectId: process.env.REACT_APP_PROJECTID,
+//     projectId: process.env.REACT_APP_PROJECTID,
   
-    storageBucket: process.env.REACT_APP_STORAGE,
+//     storageBucket: process.env.REACT_APP_STORAGE,
   
-    messagingSenderId: process.env.REACT_APP_SENDERID,
+//     messagingSenderId: process.env.REACT_APP_SENDERID,
   
-    appId: process.env.REACT_APP_APPID,
+//     appId: process.env.REACT_APP_APPID,
   
-    measurementId: process.env.REACT_APP_MEASUREMENTID,
+//     measurementId: process.env.REACT_APP_MEASUREMENTID,
   
-  };
+//   };
 
 
-// Initialize Firebase
+// // Initialize Firebase
 
-const app = initializeApp(firebaseConfig);
+// const app = initializeApp(firebaseConfig);
 
-const database = getDatabase(app);
+// export const database = getDatabase(app);
 
-
-const analytics = getAnalytics(app);
+// const analytics = getAnalytics(app);
