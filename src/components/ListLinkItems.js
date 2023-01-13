4b585d81db074f3cd4e04028062edff6cf3043dc
Vidit Khandelwal <viditk17@gmail.com>
2023-01-13 22:17:55 +0530

    removed error

--- src/components/ListLinkItems.js
+++ src/components/ListLinkItems.js
@@ -29,9 +29,7 @@ const ListLinkItems = () => {
     return (
       <div className="mt-7 mb-5 flex w-10/12 flex-row items-center justify-between space-x-2 rounded-md border border-sky-500 pt-1 pb-1 pl-2 pr-2 lg:w-1/3">
         <div className="flex flex-col items-start">
-          <div className="text-xl font-semibold text-white">
-            Fetching Links
-          </div>
+          <div className="text-xl font-semibold text-white">Fetching Links</div>
           <div className="text-xs text-slate-400">
             Offline capabilities coming soon.
           </div>
