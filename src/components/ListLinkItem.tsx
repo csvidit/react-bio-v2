7dee133de38d895dcd250fedbd70651d923dbd77
Vidit Khandelwal <viditk17@gmail.com>
2023-02-03 16:35:11 -0500

    refining

--- src/components/ListLinkItem.tsx
+++ src/components/ListLinkItem.tsx
@@ -4,14 +4,14 @@ import ListLinkText from "./ListLinkText"
 import ListLinkArrow from "./ListLinkArrow"
 import React from "react"
 
-const LinkListItem = (props) => {
+const LinkListItem = (props: { linkType: string; link: string; linkText: string}) => {
   const linkType = props.linkType;
   const link = props.link;
   const linkText = props.linkText;
 
   return (
-    <a href={link} target="_blank" className="z-10 snap-end flex flex-row w-full justify-center" rel="noreferrer">
-      <div className="lit-link flex flex-row mt-5 bg-slate-800 w-10/12 lg:w-1/3 justify-between border border-slate-800 justify-self-center self-center rounded-md p-4 hover:border-sky-500">
+    <a href={link} target="_blank" className="z-10 snap-end flex w-10/12 lg:w-1/3 flex-row justify-center" rel="noreferrer">
+      <div className="lit-link flex flex-row mt-5 bg-slate-800 w-full  justify-between border border-slate-800 justify-self-center self-center rounded-md p-4 hover:border-sky-500">
         <ListLinkLogo type={linkType}></ListLinkLogo>
         <div className="lit-link-text flex flex-col">
           <ListLinkType type={linkType}></ListLinkType>
--- src/components/ListLinkItemsContainer.tsx
+++ src/components/ListLinkItemsContainer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const ListLinkItemsContainer = (props: { children: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | React.ReactFragment | React.ReactPortal | null | undefined; }) =>
 {
-    return (<div className="w-full">{props.children}</div>);
+    return (<div className="w-full flex flex-col items-center">{props.children}</div>);
 }
 
 export default ListLinkItemsContainer;
